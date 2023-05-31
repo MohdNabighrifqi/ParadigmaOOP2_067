74c51784e911fca2e4018d0c1ddb96cabe19209f
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-05-31 19:39:40 +0700

    membuat class pelajar

--- Multiwaris2/Multiwaris2.cpp
+++ Multiwaris2/Multiwaris2.cpp
@@ -7,10 +7,12 @@ public:
 	string nama;
 
 	orang(string pNama) :
-		nama(pNama) {
+		nama(pNama) 
+	{
 		cout << "Orang dibuat\n" << endl;
 	}
-	~orang() {
+	~orang() 
+	{
 		cout << "Orang dihapus\n" << endl;
 	}
 };
@@ -20,10 +22,29 @@ public:
 	string JenisKelamin;
 
 	manusia(string pJenisKelamin) :
-		JenisKelamin(pJenisKelamin) {
+		JenisKelamin(pJenisKelamin)
+	{
 		cout << "Manusia dibuat\n" << endl;
 	}
 	~manusia() {
 		cout << "Manusia dihapus\n" << endl;
+	}
+};
+
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "Pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return " Hallo, nama saya " + nama + " dengan jenis kelamin " + JenisKelamin + " dari sekolah" + sekolah + "\n\n";
 	}
 };

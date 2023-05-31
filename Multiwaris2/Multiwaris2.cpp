e48b29cd064712b2268ad7ddb8dc033d23005b45
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-05-31 19:32:24 +0700

    membuat class orang

---
+++ Multiwaris2/Multiwaris2.cpp
@@ -0,0 +1,16 @@
+include<iostream>
+#include<string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "Orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "Orang dihapus\n" << endl;
+	}
+};
